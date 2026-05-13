d40ab3c98174b6f20f5d527407e660813e6a2e59
ryanNS3 <ryansantosn333@gmail.com>
2026-05-13 19:01:08 -0300

    feat: rota de teste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PrimaryButton } from './shared/components/buttons/primary-button/primary-button';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PrimaryButton,
+  },
+];
