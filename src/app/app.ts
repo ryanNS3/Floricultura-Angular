edd4c0599b697d414fee3a775119dfb01ff629c5
ryanNS3 <ryansantosn333@gmail.com>
2026-05-13 19:01:26 -0300

    feat: adicionando importações

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimaryButton } from './shared/components/buttons/primary-button/primary-button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrimaryButton],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('floricultura-angular');
