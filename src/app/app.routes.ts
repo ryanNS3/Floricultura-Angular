58a7217ecd03584d154cce54ed6c0c7f5f09feee
ryanNS3 <ryansantosn333@gmail.com>
2026-05-14 19:31:16 -0300

    feat: definindo rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,31 @@
 import { Routes } from '@angular/router';
 import { PrimaryButton } from './shared/components/buttons/primary-button/primary-button';
+import { HomeAdmin } from './features/adminPage/home-admin/home-admin';
+import { LandingPage } from './features/buyPage/landing-page/landing-page';
+import { Login } from './features/auth/login/login';
+import { Cadastro } from './features/auth/cadastro/cadastro';
 
 export const routes: Routes = [
   {
     path: '',
-    component: PrimaryButton,
+    component: LandingPage,
   },
+
+  {
+    path: 'homeAdmin',
+    component: HomeAdmin
+  },
+
+  {
+    path: 'login',
+    component: Login
+  },
+
+  {
+    path: 'cadastro',
+    component: Cadastro
+  }
+
+
+
 ];
