21f837d70ca9c26cca608b11d97e7a257604f433
ryanNS3 <ryansantosn333@gmail.com>
2026-05-14 19:32:06 -0300

    feat: página home do admin

---
+++ src/app/features/adminPage/home-admin/home-admin.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-home-admin",
+  imports: [],
+  templateUrl: "./home-admin.html",
+  styleUrl: "./home-admin.css",
+})
+export class HomeAdmin {}
