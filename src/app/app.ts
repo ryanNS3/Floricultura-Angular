2eb899378f590ea54a47e48d2b7c0ad0c54c9030
ryanNS3 <ryansantosn333@gmail.com>
2026-05-14 19:31:47 -0300

    feat: importando paginas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PrimaryButton } from './shared/components/buttons/primary-button/primary-button';
+import { HomeAdmin } from './features/adminPage/home-admin/home-admin';
+import { LandingPage } from './features/buyPage/landing-page/landing-page';
+import { Login } from './features/auth/login/login';
+import { Cadastro } from './features/auth/cadastro/cadastro';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PrimaryButton],
+  imports: [RouterOutlet, HomeAdmin, LandingPage, Login, Cadastro],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
