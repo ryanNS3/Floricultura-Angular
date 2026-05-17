c4e270678068be9ccc99e09cec67d159d0632287
ryanNS3 <ryansantosn333@gmail.com>
2026-05-16 21:19:54 -0300

    feat: carregando dados dinamicos

--- src/app/features/adminPage/home-admin/home-admin.ts
+++ src/app/features/adminPage/home-admin/home-admin.ts
@@ -1,23 +1,37 @@
-import { Component, inject, NgModule } from "@angular/core";
+import { Component, inject, NgModule, OnInit, signal } from "@angular/core";
 import { Produto } from "../../../service/@Type/Produto";
 import { HttpClient } from "@angular/common/http";
 import { ProdutoService } from "../../../service/produto/produto-service";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-home-admin",
-  imports: [],
+  imports: [RouterLink],
   templateUrl: "./home-admin.html",
   styleUrl: "./home-admin.css",
 })
-export class HomeAdmin {
+export class HomeAdmin implements OnInit {
   private produtoService = inject(ProdutoService)
-  listaProdutos:Produto[]= []  
-  nomeTabela = "Produtossss"
+  listaProdutos = signal<Produto[]>([])  
   httpClient = inject(HttpClient)
 
-  ngOnInit(){
+ excluirProduto(id: string){
+ 
+  this.produtoService.deleteProduto(id).subscribe({
+        next: () => {
+          // Atualiza o Signal removendo o item da lista local
+          this.listaProdutos.update(produtos => 
+            produtos.filter(p => p.id !== id)
+          );
+          console.log('Produto removido!');
+        },
+        error: (err) => alert(err.message)
+      });
+ }
+
+  ngOnInit(): void{
     this.produtoService.getProdutos().subscribe({
-      next : (dados) => this.listaProdutos = dados,
+      next : (dados) => this.listaProdutos.set(dados),
       error: (err) => console.log(err)
     })
   }
